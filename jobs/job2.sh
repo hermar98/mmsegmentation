5820f1986aba9a1b9501126a422668f0f2ce1e14
Herman Ryen Martinsen <hermanrm@vcvm18.idi.ntnu.no>
2021-11-14 13:19:07 +0100

    Updated scripts for testing an array job

---
+++ jobs/job2.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 PORT=29501 ./tools/dist_train.sh configs/swin/upernet_swin_tiny_patch4_window7_300x300_10k_ade20k_pretrain_224x224_1K_single_gpu.py 1
--- jobs/single_gpu_training.sh
+++
@@ -1,1 +0,0 @@
-./tools/dist_train.sh configs/swin/upernet_swin_tiny_patch4_window7_300x300_10k_ade20k_pretrain_224x224_1K_single_gpu.py 1
