179ec11ec7489d810e300eb911da7aec2524ab3f
Herman Ryen Martinsen <hermanrm@vcvm18.idi.ntnu.no>
2021-11-26 02:23:15 +0100

    Fixed typo in model config

--- configs/swin/upernet_swin_base_patch4_window7_769x769_80k_cityscapes_ade20k_pretrain_224x224_22K.py
+++ configs/swin/upernet_swin_base_patch4_window7_769x769_80k_cityscapes_ade20k_pretrain_224x224_22K.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 
 model = dict(
-    pretrained='pretrain/swin_base_patch4_window7_224_22.pth',
+    pretrained='pretrain/swin_base_patch4_window7_224_22k.pth',
     backbone=dict(
         embed_dims=128,
         depths=[2, 2, 18, 2],
