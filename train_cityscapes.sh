8e7318a5df0b3e3c2f03f606ec67b627949d4863
Herman Ryen Martinsen <hermanrm@vcvm18.idi.ntnu.no>
2021-11-26 03:14:09 +0100

    Update training script

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=1 PORT=29501 ./tools/dist_train.sh configs/swin/upernet_swin_base_patch4_window7_769x769_80k_cityscapes_ade20k_pretrain_224x224_22K.py 1
+CUDA_VISIBLE_DEVICES=1 PORT=29501 ./tools/dist_train.sh configs/swin/upernet_swin_base_patch4_window7_512x1024_80k_cityscapes_ade20k_pretrain_224x224_22K.py 1
